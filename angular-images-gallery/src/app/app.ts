5097b0d398aeb6f11b02a5cd1152d542b000455d
albert-dev <al96@hotmail.es>
2026-06-30 10:46:56 +0200

    feat: "Installed Tailwindcss and PrimeNG and created a button component to try PrimeNG"

--- angular-images-gallery/src/app/app.ts
+++ angular-images-gallery/src/app/app.ts
@@ -1,13 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Gallery } from './components/gallery/gallery';
+import { ButtonDirective } from 'primeng/button';
+
+@Component({
+    selector: 'button-demo',
+    template: `<button pButton>Check</button>`,
+    imports: [ButtonDirective]
+})
+export class ButtonDemo {}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Gallery],
+  imports: [RouterOutlet, Gallery, ButtonDemo],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-images-gallery');
 }
+
