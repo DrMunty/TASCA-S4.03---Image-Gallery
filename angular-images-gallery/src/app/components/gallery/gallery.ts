76874e62075191148abcd7192cd387664f0b3c67
albert-dev <al96@hotmail.es>
2026-07-01 11:40:00 +0200

    "fix: improved pictures' resolution"

--- angular-images-gallery/src/app/components/gallery/gallery.ts
+++ angular-images-gallery/src/app/components/gallery/gallery.ts
@@ -14,61 +14,61 @@ export class Gallery {
   myGallery = signal<Image[]>([
     {
       id: '1',
-      src: 'https://picsum.photos/id/237/200/300',
+      src: 'https://picsum.photos/id/237/500/600',
       alt: 'A picture of a black dog',
     },
 
     {
       id: '2',
-      src: 'https://picsum.photos/id/22/200/300',
+      src: 'https://picsum.photos/id/22/500/600',
       alt: 'A lonely man crossing a street'
     },
 
     {
       id: '3',
-      src: 'https://picsum.photos/id/120/200/300',
+      src: 'https://picsum.photos/id/120/500/600',
       alt: 'A picture of starry night sky'
     },
 
     {
       id: '4',
-      src: 'https://picsum.photos/id/472/200/300',
+      src: 'https://picsum.photos/id/472/500/600',
       alt: 'A picture of a foggy forest'
     },
 
     {
       id: '5',
-      src: 'https://picsum.photos/id/582/200/300',
+      src: 'https://picsum.photos/id/582/500/600',
       alt: 'A picture of a wolf'
     },
 
     {
       id: '6',
-      src: 'https://picsum.photos/id/619/200/300',
+      src: 'https://picsum.photos/id/619/500/600',
       alt: 'A picture of a red phone cabin'
     },
 
     {
       id: '7',
-      src: 'https://picsum.photos/id/655/200/300',
+      src: 'https://picsum.photos/id/655/500/600',
       alt: 'A picture of an old blue and white van'
     },
 
     {
       id: '8',
-      src: 'https://picsum.photos/id/685/200/300',
+      src: 'https://picsum.photos/id/685/500/600',
       alt: 'A picture of a man sitting on a rock above the clouds'
     },
 
     {
       id: '9',
-      src: 'https://picsum.photos/id/742/200/300',
+      src: 'https://picsum.photos/id/742/500/600',
       alt: 'A picture of the statue of liberty'
     },
 
     {
       id: '10',
-      src: 'https://picsum.photos/id/832/200/300',
+      src: 'https://picsum.photos/id/832/500/600',
       alt: 'A picture of a woman reading'
     },
   ])
